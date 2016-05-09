cca3486dfd002775067ce59cba6a7edb0886287e
xuerong93 <hupodeshuo@gmail.com>
2016-05-09 19:01:56 -0400

    [8. String to Integer (atoi)][Accepted]committed by LeetCode Extension

--- 8-String-to-Integer-(atoi)/solution.java
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -1,27 +1,27 @@
 public class Solution {
     public int myAtoi(String str) {
         if(str==null || str.length()==0) return 0;
-        str = str.replace(" ","");
+        str = str.trim();
         double result = 0 ;
         if(str.charAt(0)-'0'>=0 && str.charAt(0)<='9'){
             int i=0;
-            while(str.length()>0 && str.charAt(i)-'0'>=0 && str.charAt(i)-'0'<=9){
-                result = 10*result+str.charAt(i)-'0';
+            while(str.length()>i && str.charAt(i)-'0'>=0 && str.charAt(i)-'0'<=9){
+                result = 10*result+(str.charAt(i)-'0');
                 i++;
             }
         }
         if(str.charAt(0)=='+' || str.charAt(0)=='-'){
             if(str.charAt(0)=='+'){
                 int i=1;
-                while(str.length()>0 && str.charAt(i)-'0'>=0 && str.charAt(i)-'0'<=9){
-                    result = 10*result+str.charAt(i)-'0';
+                while(str.length()>i && str.charAt(i)-'0'>=0 && str.charAt(i)-'0'<=9){
+                    result = 10*result+(str.charAt(i)-'0');
                     i++;
                 }
             }
             else{
                 int i=1;
-                while(str.length()>0 && str.charAt(i)-'0'>=0 && str.charAt(i)-'0'<=9){
-                    result = 10*result-str.charAt(i)-'0';
+                while(str.length()>i && str.charAt(i)-'0'>=0 && str.charAt(i)-'0'<=9){
+                    result = 10*result-(str.charAt(i)-'0');
                     i++;
                 }
             }
