1022e05c5d8f380bf3e97b4a7a2e50dca88b9f79
xuerong93 <hupodeshuo@gmail.com>
2016-05-09 18:53:34 -0400

    [8. String to Integer (atoi)][Runtime Error]committed by LeetCode Extension

--- 8-String-to-Integer-(atoi)/solution.java
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -5,7 +5,7 @@ public class Solution {
         double result = 0 ;
         if(str.charAt(0)-'0'>=0 && str.charAt(0)<='9'){
             int i=0;
-            while(str.length()>0 && str.charAt(i)-'0'>=0 && str.charAt(i)<='9'){
+            while(str.length()>0 && str.charAt(i)-'0'>=0 && str.charAt(i)-'0'<=9){
                 result = 10*result+str.charAt(i)-'0';
                 i++;
             }
@@ -13,14 +13,14 @@ public class Solution {
         if(str.charAt(0)=='+' || str.charAt(0)=='-'){
             if(str.charAt(0)=='+'){
                 int i=1;
-                while(str.length()>0 && str.charAt(i)-'0'>=0 && str.charAt(i)<='9'){
+                while(str.length()>0 && str.charAt(i)-'0'>=0 && str.charAt(i)-'0'<=9){
                     result = 10*result+str.charAt(i)-'0';
                     i++;
                 }
             }
             else{
                 int i=1;
-                while(str.length()>0 && str.charAt(i)-'0'>=0 && str.charAt(i)<='9'){
+                while(str.length()>0 && str.charAt(i)-'0'>=0 && str.charAt(i)-'0'<=9){
                     result = 10*result-str.charAt(i)-'0';
                     i++;
                 }
