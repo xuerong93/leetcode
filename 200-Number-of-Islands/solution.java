d06f62a1b3988da435f157d569f0e3d32b24adc6
xuerong93 <hupodeshuo@gmail.com>
2016-05-10 01:23:39 -0400

    [200. Number of Islands][Runtime Error]committed by LeetCode Extension

---
+++ 200-Number-of-Islands/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int numIslands(char[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0].length == 0) return 0;
+        int count = 0;
+        for(int i = 0 ;i < grid.length; i++){
+            for(int j = 0;j < grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                    dfs(grid,i,j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public void dfs(char[][] grid, int i,int j){
+        if(i<0 || i>= grid.length || j<0 || j>= grid[0].length || grid[i][j]!='1') return;
+        dfs(grid,i-1,j);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i,j+1);
+    }
+}
