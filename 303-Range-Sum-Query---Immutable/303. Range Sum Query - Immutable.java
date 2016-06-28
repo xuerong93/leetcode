5a571d734520085aa3a9248ae87f156d0067f024
xuerong93 <hupodeshuo@gmail.com>
2016-06-28 19:15:59 -0400

    [303. Range Sum Query - Immutable][Accepted]committed by LeetCode Extension

--- 303-Range-Sum-Query---Immutable/303. Range Sum Query - Immutable.java
+++ 303-Range-Sum-Query---Immutable/303. Range Sum Query - Immutable.java
@@ -1,17 +1,17 @@
 public class NumArray {
     private int[] dp;
     public NumArray(int[] nums) {
-        dp=new int[nums.length];
+        dp=new int[nums.length+1];
         if(nums.length>0){
-            dp[0]=nums[0];
-            for(int i=1;i<nums.length;i++){
-                dp[i]=dp[i-1]+nums[i];
+            dp[0]=0;
+            for(int i=0;i<nums.length;i++){
+                dp[i+1]=dp[i]+nums[i];
             }
         }
     }
 
     public int sumRange(int i, int j) {
-        return (j-i<0)?0:(dp[j]-dp[i-1]);
+        return (j-i<0)?0:(dp[j+1]-dp[i]);
     }
 }
 
