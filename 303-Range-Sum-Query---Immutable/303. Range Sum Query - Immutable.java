10b8a6e519a944d708b5d8bd3d9b9d6f3a8c7ded
xuerong93 <hupodeshuo@gmail.com>
2016-06-28 19:11:01 -0400

    [303. Range Sum Query - Immutable][Runtime Error]committed by LeetCode Extension

--- 303-Range-Sum-Query---Immutable/303. Range Sum Query - Immutable.java
+++ 303-Range-Sum-Query---Immutable/303. Range Sum Query - Immutable.java
@@ -4,14 +4,14 @@ public class NumArray {
         dp=new int[nums.length];
         if(nums.length>0){
             dp[0]=nums[0];
-            for(int i=i;i<nums.length;i++){
+            for(int i=1;i<nums.length;i++){
                 dp[i]=dp[i-1]+nums[i];
             }
         }
     }
 
     public int sumRange(int i, int j) {
-        return (j-i>0)?0:(dp[j]-dp[i]);
+        return (j-i<0)?0:(dp[j]-dp[i-1]);
     }
 }
 
