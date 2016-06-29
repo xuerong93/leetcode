4e3d28227d6523a963cd8762351c5bcd104c8f64
xuerong93 <hupodeshuo@gmail.com>
2016-06-29 13:35:36 -0400

    [141. Linked List Cycle][Runtime Error]committed by LeetCode Extension

--- 141-Linked-List-Cycle/141. Linked List Cycle.java
+++ 141-Linked-List-Cycle/141. Linked List Cycle.java
@@ -13,7 +13,7 @@ public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode p1=head;
         ListNode p2=head;
-        while(p2!=null){
+        while(p2.next!=null){
             p1=p1.next;
             p2=p2.next.next;
             if(p1==p2) return true;
