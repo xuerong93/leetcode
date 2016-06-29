fe98451562c8f96cdb2705688dc4aa58fbcabde2
xuerong93 <hupodeshuo@gmail.com>
2016-06-29 14:03:49 -0400

    [203. Remove Linked List Elements][Accepted]committed by LeetCode Extension

---
+++ 203-Remove-Linked-List-Elements/203. Remove Linked List Elements.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head== null) return null;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode p1=dummy;
+        ListNode p2=p1.next;
+        while(p2!=null){
+            while(p2!=null && p2.val==val){
+                p2=p2.next;
+                p1.next=p2;
+            }
+            if(p2==null) break;
+            p1=p2;
+            p2=p2.next;
+        }
+        return dummy.next;
+    }
+}
