c51eb4f561dc39d42a8c4ef17bca874d9e62e817
xuerong93 <hupodeshuo@gmail.com>
2016-07-07 14:08:10 -0400

    [6. ZigZag Conversion][Compile Error]committed by LeetCode Extension

---
+++ 6-ZigZag-Conversion/solution2.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public String convert(String s, int numRows) {
+        
+        if(numRows==1 || numRows>s.length()) return s;
+        StringBuilder res=new StringBuilder();
+        int count=0;
+        for(int i=0;i<numRows;i++){
+            int interval=2*(numRows-1)-2*i;
+            for(int j=i;j<length;j+=2*(numRows-1)){
+                res.append(s.charAt(j));
+                count++;
+                if(interval>0 &&　interval<2*(numRows-1) && j+interval<length && count<length ){
+                    res.append(s.charAt(j+interval));
+                    count++;
+                }
+            }
+        }
+        return res.toString();
+    
+    }
+}
