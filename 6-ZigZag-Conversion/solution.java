fedcaab4fbfc1319a018094bcee21239d08cdff7
xuerong93 <hupodeshuo@gmail.com>
2016-07-07 13:49:30 -0400

    [6. ZigZag Conversion][Accepted]committed by LeetCode Extension

---
+++ 6-ZigZag-Conversion/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows==1 || numRows>s.length()) return s;
+        StringBuilder res=new StringBuilder();
+        for(int j=0;j<numRows;j++){
+            for(int i=0;i<s.length();i++){
+                if(i%(2*numRows-2)==j || i%(2*numRows-2)==2*numRows-2-j){
+                    res.append(s.charAt(i));
+                }
+            }
+        }
+        return res.toString();
+    
+    }
+}
