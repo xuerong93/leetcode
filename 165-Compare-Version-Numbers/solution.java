6f8abc3019ef0a0b3f5b423d3059734c5b3c16b8
xuerong93 <hupodeshuo@gmail.com>
2016-07-07 19:58:40 -0400

    [165. Compare Version Numbers][Time Limit Exceeded]committed by LeetCode Extension

--- 165-Compare-Version-Numbers/solution.java
+++ 165-Compare-Version-Numbers/solution.java
@@ -1,29 +1,21 @@
 public class Solution {
     public int compareVersion(String version1, String version2) {
-        
         String[] part1=version1.split("\\.");
         String[] part2=version2.split("\\.");
-        if(Integer.parseInt(part1[0])>Integer.parseInt(part2[0])) return 1;
-        else if(Integer.parseInt(part1[0])<Integer.parseInt(part2[0])) return -1;
-        else{
-            for(int i=0;i<Math.min(part1[1].length(),part2[1].length());i++){
-                if(part1[1].charAt(i)>part2[1].charAt(i)) return 1;
-                else if(part1[1].charAt(i)<part2[1].charAt(i)) return -1;
+        int i=0;
+        while(i<part1.length || i<part2.length){
+            while(i<part1.length && i<part2.length){
+                if(Integer.parseInt(part1[i])>Integer.parseInt(part2[i])) return 1;
+                else if(Integer.parseInt(part1[i])<Integer.parseInt(part2[i])) return -1;
+                i++;
             }
-            if(part1[1].length()>part2[1].length()){
-                for(int i = part2[1].length();i<part1[1].length();i++){
-                    if(part1[1].charAt(i)>'0') return 1;
-                }
-                return 0;
+            if(i<part1.length){
+                if(Integer.parseInt(part1[i])>0) return 1;
             }
-            if(part1[1].length()<part2[1].length()){
-                for(int i = part1[1].length();i<part2[1].length();i++){
-                    if(part2[1].charAt(i)>'0') return -1;
-                }
-                return 0;
+            if(i<part2.length){
+                if(Integer.parseInt(part2[i])>0) return -1;
             }
         }
-
         return 0;
     }
 }
