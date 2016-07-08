43708e3260b6fbcf3e5aecd48e207ea6fd46290e
xuerong93 <hupodeshuo@gmail.com>
2016-07-07 22:44:10 -0400

    [20. Valid Parentheses][Compile Error]committed by LeetCode Extension

---
+++ 20-Valid-Parentheses/solution2.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<Character>();
+        
+        for(int i=0;i<s.length();i++){
+            if(stack.isEmpty() && (s.charAt(i)==')' || s.charAt(i)=='}' || s.charAt(i)==']')) return false;
+            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+                stack.push(s.charAt(i));
+                continue;
+            }
+            if(!((stack.pop()=='(' && s.charAt(i)==')') || (stack.pop()=='{' &&s.charAt(i)=='}') || (stack.pop()=='[' &&s.charAt(i)==']'))}{
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
