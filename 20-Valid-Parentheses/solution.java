e920ead25d42f1b6bb7a33642e34a11bf40f475c
xuerong93 <hupodeshuo@gmail.com>
2016-07-07 22:31:16 -0400

    [20. Valid Parentheses][Runtime Error]committed by LeetCode Extension

--- 20-Valid-Parentheses/solution.java
+++ 20-Valid-Parentheses/solution.java
@@ -1,6 +1,7 @@
 public class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack=new Stack<Character>();
+        if(s.charAt(0)==')' || s.charAt(0)=='}' || s.charAt(0)==']') return false;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
                 stack.push(s.charAt(i));
