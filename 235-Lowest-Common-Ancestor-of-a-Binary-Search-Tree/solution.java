c6556122d8fb31856a8790a5b18aabea10cb44e3
xuerong93 <hupodeshuo@gmail.com>
2016-07-12 20:51:26 -0400

    [235. Lowest Common Ancestor of a Binary Search Tree][Accepted]committed by LeetCode Extension

--- 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.java
+++ 235-Lowest-Common-Ancestor-of-a-Binary-Search-Tree/solution.java
@@ -12,5 +12,6 @@ public class Solution {
         if((root.val>=p.val &&root.val<=q.val) || (root.val<=p.val &&root.val>=q.val)) return root;
         if((root.val>p.val &&root.val>q.val))  return lowestCommonAncestor(root.left, p, q);
         if((root.val<p.val &&root.val<q.val))  return lowestCommonAncestor(root.right, p, q);
+        return root;
     }
 }
