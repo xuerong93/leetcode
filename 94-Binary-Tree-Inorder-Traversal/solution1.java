43072217da1283bcde86aaac87d701d1ff69151b
xuerong93 <hupodeshuo@gmail.com>
2016-07-14 16:16:19 -0400

    [94. Binary Tree Inorder Traversal][Compile Error]committed by LeetCode Extension

---
+++ 94-Binary-Tree-Inorder-Traversal/solution1.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    List<Integer> res = new ArrayList<Integer>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return res;
+        
+    }
+    public void inorder(TreeNode root){
+        if(root==null) return;
+        if(root.left!=null) inorder(root.left);
+        list.add(root.val);
+        if(root.right!=null) inorder(root.right);
+    }
+}
