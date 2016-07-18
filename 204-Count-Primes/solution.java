00a4585c296a7711df3a6263616658897b1fef12
xuerong93 <hupodeshuo@gmail.com>
2016-07-18 00:05:19 -0400

    [204. Count Primes][Accepted]committed by LeetCode Extension

--- 204-Count-Primes/solution.java
+++ 204-Count-Primes/solution.java
@@ -1,15 +1,20 @@
 public class Solution {
     public int countPrimes(int n) {
-        if(n<=2) return 0;
-        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
-        map.put(0,2);
+        int[] res=new int[n];//default is false
         for(int i=2;i<n;i++){
-            int j=0;
-            for(;j<map.size();j++){
-                if(i%map.get(j)==0) break;
-            }
-            if(j==map.size()) map.put(map.size(),i);
+            res[i]=1;   //1 means true, 0 means false
         }
-        return map.size();
+        for(int i=2;i*i<n;i++){
+            if(res[i]==0) continue;
+            for(int j=i*i;j<n;j+=i){
+                res[j]=0;
+            }
+            
+        }
+        int count=0;
+        for(int i=2;i<n;i++){
+            if(res[i]==1) count++;
+        }
+        return count;
     }
 }
