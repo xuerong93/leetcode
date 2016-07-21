fb9989fcda23b338b9124f01500a951c06648618
xuerong93 <hupodeshuo@gmail.com>
2016-07-21 16:52:13 -0400

    [278. First Bad Version][Time Limit Exceeded]committed by LeetCode Extension

--- 278-First-Bad-Version/solution.java
+++ 278-First-Bad-Version/solution.java
@@ -3,12 +3,10 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        if(n<=0) return 0;
-        if(isBadVersion(1)) return 1;
         int start=1;
         int end=n;
         while(start<=end){
-            int mid=(start+end)/2;
+            int mid=(end-start)>>2+start;
             if(isBadVersion(mid)){
                 //if(mid-1>=start && !isBadVersion(mid-1)) return mid;
                 end=mid-1;
