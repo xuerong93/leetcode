330ba31f0cc6e2c90e7ba0a2cc3f274d33f6a400
xuerong93 <hupodeshuo@gmail.com>
2016-07-21 17:02:39 -0400

    [278. First Bad Version][Compile Error]committed by LeetCode Extension

--- 278-First-Bad-Version/solution.java
+++ 278-First-Bad-Version/solution.java
@@ -4,12 +4,12 @@
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
         int start = 1, end = n;
- +        while (start + 1 < end) {
+ +        while (start <= end) {
  +            int mid = start + (end - start) / 2;
  +            if (isBadVersion(mid)) {
- +                end = mid;
+ +                end = mid-1;
  +            } else {
- +                start = mid;
+ +                start = mid+1;
  +            }
           }
  -        return end+1 ;
