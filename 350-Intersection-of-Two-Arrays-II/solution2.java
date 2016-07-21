ed7ccf57b1bbbdf1883bdb46315771316fb14c78
xuerong93 <hupodeshuo@gmail.com>
2016-07-21 15:42:22 -0400

    [350. Intersection of Two Arrays II][Compile Error]committed by LeetCode Extension

---
+++ 350-Intersection-of-Two-Arrays-II/solution2.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if(nums1==null || nums2==null) return int[];
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i=0;
+        int j =0;
+        int index=0;
+        int[] temp=new int[nums1.length];
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]==nums2[j]){
+                temp[index++]=nums2[j];
+                i++;
+                j++;
+            } 
+            else if(nums1[i]<nums2[j]) i++;
+            else j++;
+        }
+        int[] res=new int[index];
+        for(int i=0;i<index;i++){
+            res[i]=temp[i];
+        }
+        return res;
+    }
+}
