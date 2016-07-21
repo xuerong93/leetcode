acf5e92c83fce3b19d0e6651f62b797eccb0a793
xuerong93 <hupodeshuo@gmail.com>
2016-07-21 17:04:51 -0400

    [278. First Bad Version][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 278-First-Bad-Version/solution3.java
@@ -0,0 +1,17 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start = 1, end = n;
+         while (start <= end) {
+             int mid = (end + start) / 2;//((end-start)>>1)+start
+             if (isBadVersion(mid)) {
+                 end = mid-1;
+             } else {
+                 start = mid+1;
+             }
+          }
+        return end+1 ;
+    }
+}
