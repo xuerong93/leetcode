beec199c6409baa74463f19668551aa72b94f9a2
xuerong93 <hupodeshuo@gmail.com>
2016-07-24 18:19:39 -0400

    [89. Gray Code][Accepted]committed by LeetCode Extension

---
+++ 89-Gray-Code/solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> res=new ArrayList<Integer>();
+        if(n<0) return res;
+        res.add(0);
+        
+    }
+}
