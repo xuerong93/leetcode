0306c75cfbfdf2fcbcf7194e9781b9af069e279f
xuerong93 <hupodeshuo@gmail.com>
2016-09-02 15:33:44 -0700

    [7. Reverse Integer][Compile Error]committed by LeetCode Extension

--- 7-Reverse-Integer/solution.java
+++ 7-Reverse-Integer/solution.java
@@ -1,20 +1,12 @@
 public class Solution {
     public int reverse(int n) {
-        if(n==0) return 0;
-        boolean bol = (n<0);
-        n=Math.abs(n);
-        int reversed_n = 0;
-        while(n>0){
-            int temp = reversed_n ;
-            reversed_n = temp*10+n%10;
-            if(reversed_n/10 != temp){
-                reversed_n = 0;
-                break;
-            }
+        long temp=0;
+        while(n!=0){
+            temp = temp*10+n%10;
             n/=10;
         }
-        if(bol) reversed_n = reversed_n*(-1);
-
-        return reversed_n;
+        if(bol) temp=temp*(-1);
+        if(temp>Integer.MAX_VALUE || temp<Integer.MIN_VALUE) return 0;
+        return (int) temp;
     }
 }
