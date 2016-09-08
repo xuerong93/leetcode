b471a4582191065adce48d1ae628df3d14566116
xuerong93 <hupodeshuo@gmail.com>
2016-09-07 22:59:24 -0700

    [344. Reverse String][Accepted]committed by LeetCode Extension

--- 344-Reverse-String/solution1.java
+++ 344-Reverse-String/solution1.java
@@ -1,13 +1,16 @@
 public class Solution {
     public String reverseString(String s) {
-        if(s==null || s.length()==0 || s.length()==1) return s;
-        int length=s.length();
-        char[] store=s.toCharArray();
+        if(s==null || s.length() == 0 || s.length() == 1) return s;
+        int length = s.length();
+        char[] arr = s.toCharArray();
         for(int i=0;i<length/2;i++){
-            char temp=store[i];
-            store[i]=store[length-i-1];
-            store[length-i-1]=temp;
+            swap(arr,i,length-1-i);
         }
-        return new String(store);
+        return new String(arr);
+    }
+    public void swap(char[] arr, int start, int end){
+        char temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 }
