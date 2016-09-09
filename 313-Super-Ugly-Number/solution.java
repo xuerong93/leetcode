cdaac3a61fefc3f1e001a511c831cd26b7021d81
xuerong93 <hupodeshuo@gmail.com>
2016-09-08 22:29:47 -0700

    [313. Super Ugly Number][Accepted]committed by LeetCode Extension

--- 313-Super-Ugly-Number/solution.java
+++ 313-Super-Ugly-Number/solution.java
@@ -1,22 +1,27 @@
 public class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
-        int[] times = new int[primes.length];
-        int[] uglys = new int[n];
-        uglys[0] = 1;
+        int m = primes.length;
+         int[] factors = new int[m];
+         for(int i=0;i<m;i++){
+             factors[i] = primes[i];
+         }
+         int[] index = new int[m];
+         int[] res = new int[n];
+         res[0] = 1;
+         
+         for(int i = 1;i<n;i++){
+             int min = Integer.MAX_VALUE;
+             for(int j=0;j<m;j++){
+                 min = Math.min(min,primes[j]);
+              }
+             res[i] = min;
+             for(int j = 0;j<m;j++){
+                 if(min == primes[j]){
+                     primes[j] = res[++index[j]] * factors[j];
 
-        for (int i = 1; i < n; i++) {
-            int min = Integer.MAX_VALUE;
-            for (int j = 0; j < primes.length; j++) {
-                min = Math.min(min, primes[j] * uglys[times[j]]);
-            }
-            uglys[i] = min;
-
-            for (int j = 0; j < times.length; j++) {
-                if (uglys[times[j]] * primes[j] == min) {
-                    times[j]++;
-                }
-            }
-        }
-        return uglys[n - 1];
+                  }
+              }
+          }
+         return res[n-1];
     }
 }
