075f9edc884e261c577cfce8b77b1d502196b30d
xuerong93 <hupodeshuo@gmail.com>
2016-09-08 17:12:57 -0700

    [186. Reverse Words in a String II][Compile Error]committed by LeetCode Extension

--- 186-Reverse-Words-in-a-String-II/solution.java
+++ 186-Reverse-Words-in-a-String-II/solution.java
@@ -1,27 +1,27 @@
 public class Solution {
     public void reverseWords(char[] s) {
-        int length = s.length;
-        if(length==0 || length==1) return;
-        int i = 0;
-        int j = 0;
-        while(j<length){
-            while(j<length && s[j]==' ' && s[j]==s[j-1]){
-                j++;
-            }
-            s[i] = s[j];
+         if (s = null || s.length==0 || s.length ==1) return;
+         int length = s.length;
+         int i = 0;
+         for(int j=0;j<length;j++){
+             if(s[j]==' '){
+                 reverse(s, i, j-1);
+                 i=j+1;
+             }
+         }
+         reverse(s,i,length-1);
+         reverse(s,0,length-1);
+    }
+    public void reverse(char[] s, int i, int j){
+        while(i<j){
+            swap(s, i,j);
             i++;
-            j++;
-        }
-        for(int m=i;m<j;m++){
-            s[m]==' ';
-        }
-        for(int m=0;m<i/2;m++){
-            swap(s,0,i-1);
+            j--;
         }
     }
-    public void swap(char[] s, int i, int j){
-        char temp = s[i];
+    public void swap(char[] s,int i; int j){
+        char temp =s[i];
         s[i] = s[j];
-        s[j] temp;
+        s[j] = temp;
     }
 }
