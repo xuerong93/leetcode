90b44df7cad33e361104d8820ebe8814035aa2c9
xuerong93 <hupodeshuo@gmail.com>
2016-09-18 16:31:29 -0700

    [170. Two Sum III - Data structure design][Compile Error]committed by LeetCode Extension

---
+++ 170-Two-Sum-III---Data-structure-design/solution.java
@@ -0,0 +1,31 @@
+public class TwoSum {
+    HashMap<Integer,Integer> addNum = new HashMap<Integer,Integer>(); 
+    // Add the number to an internal data structure.
+	public void add(int number) {
+	    if(addNum.containsKey(number)){
+	        addNum.put(number,addNum.get(number)+1);
+	    }else{
+	        addNum.put(number,1);
+	    }
+	}
+
+    // Find if there exists any pair of numbers which sum is equal to the value.
+	public boolean find(int value) {
+	    for(int i: addNum){
+	        int j = value - i;
+	        if(addNum.contains(j)){
+	            if(i == j && addNum.get(i)<2){
+	                continue;
+	            }
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+}
+
+
+// Your TwoSum object will be instantiated and called as such:
+// TwoSum twoSum = new TwoSum();
+// twoSum.add(number);
+// twoSum.find(value);
