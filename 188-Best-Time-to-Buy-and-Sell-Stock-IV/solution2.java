0921b948fa0a57437a3c8f5b6db46a121ec48164
xuerong93 <hupodeshuo@gmail.com>
2016-09-18 13:53:46 -0700

    [188. Best Time to Buy and Sell Stock IV][Accepted]committed by LeetCode Extension

--- 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution2.java
+++ 188-Best-Time-to-Buy-and-Sell-Stock-IV/solution2.java
@@ -4,15 +4,18 @@ public class Solution {
         
         int days = prices.length;
         if (k >= days) return maxProfit2(prices);
-        
+        //there must be a transaction on local
         int[][] local = new int[days][k + 1];
+        //there may not be a transaction on global
         int[][] global = new int[days][k + 1];
         
         for (int i = 1; i < days ; i++) {
             int diff = prices[i] - prices[i - 1];
             
             for (int j = 1; j <= k; j++) {
+                //j-1th transaction on the first i-1 days and the jth day with one transaction or   on i-1th day it sells but on ith day it also sells, the sell action can be combined 
                 local[i][j] = Math.max(global[i - 1][j - 1]+Math.max(diff,0), local[i - 1][j] + diff);
+                //global benefit may be the profit until i-1th day or the jth transcation on ith day. 
                 global[i][j] = Math.max(global[i - 1][j], local[i][j]);
              }
         }
