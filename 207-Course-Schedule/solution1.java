c826906cbe1db48685225208c7cbe2cdb9912683
xuerong93 <hupodeshuo@gmail.com>
2016-09-19 01:02:04 -0700

    [207. Course Schedule][Compile Error]committed by LeetCode Extension

--- 207-Course-Schedule/solution1.java
+++ 207-Course-Schedule/solution1.java
@@ -1,44 +1,31 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        //bfs with queue
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+        for(int i=0;i<numCourses;i++){
+            list.add(new ArrayList<Integer>());
+        }
         
-        //bfs
-       int len = prerequisites.length;
- 
-    if(numCourses == 0 || len == 0){
-        return true;
-    }
- 
-    // counter for number of prerequisites
-    int[] pCounter = new int[numCourses];
-    for(int i=0; i<len; i++){
-        pCounter[prerequisites[i][0]]++;
-    }
- 
-    //store courses that have no prerequisites
-    LinkedList<Integer> queue = new LinkedList<Integer>();
-    for(int i=0; i<numCourses; i++){
-        if(pCounter[i]==0){
-            queue.add(i);
+        int[] preNum = new int[numCourses];
+        for(int i=0;i<prerequisites.length;i++){
+            list.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            preNum[prerequisites[i][0]]++;
         }
-    }
- 
-    // number of courses that have no prerequisites
-    int numNoPre = queue.size();
- 
-    while(!queue.isEmpty()){
-        int top = queue.remove();
-        for(int i=0; i<len; i++){
-            // if a course's prerequisite can be satisfied by a course in queue
-            if(prerequisites[i][1]==top){
-                pCounter[prerequisites[i][0]]--;
-                if(pCounter[prerequisites[i][0]]==0){
-                    numNoPre++;
-                    queue.add(prerequisites[i][0]);
-                }
+        
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for(int i=0;i<numCourses;i++){
+            if(preNum[i]==0) queue.add(i);
+        }
+        int count = numCourses;
+        while(!queue.isEmpty()){
+            int out = queue.poll();
+            for(int i : out){
+                if(--preNum[i]==0){
+                    queue.add(i);
+                } 
             }
+            count--;
         }
+        return count==0;
     }
- 
-    return numNoPre == numCourses;
-}
 }
