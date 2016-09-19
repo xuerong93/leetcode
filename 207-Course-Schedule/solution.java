164dca45a8a1837b16d334ac389b61f73e520925
xuerong93 <hupodeshuo@gmail.com>
2016-09-18 17:39:21 -0700

    [207. Course Schedule][Accepted]committed by LeetCode Extension

--- 207-Course-Schedule/solution.java
+++ 207-Course-Schedule/solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        //dfs
+        //dfs，DFS的解法，也需要建立有向图，还是用二维数组来建立，和BFS不同的是，我们像现在需要一个一维数组visit来记录访问状态，大体思路是，先建立好有向图，然后从第一个门课开始，找其可构成哪门课，暂时将当前课程标记为已访问，然后对新得到的课程调用DFS递归，直到出现新的课程已经访问过了，则返回false，没有冲突的话返回true，然后把标记为已访问的课程改为未访问。
        int len = prerequisites.length;
  
     if(numCourses == 0 || len == 0){
